b907e06f77cb22bd88b4e843fe285863af6245e1
Anderson <andersoncsilva850@gmail.com>
2023-09-04 20:17:13 -0300

    js
    colocando a logica e as funções

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,72 @@
-const previousOperationText = document.querySelector("previous-operation");
-const currentOperationText = document.querySelector("current-operation");
+const previousOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
 const buttons = document.querySelectorAll("#buttons-container button");
 
 
 //class
-class Calculator{}
+class Calculator{
+    constructor(previousOperationText, currentOperationText){
+        this.previousOperationText = previousOperationText;
+        this.currentOperationText = currentOperationText;
+        this.currentOperation = "";
 
-buttons.forEach ((btn) =>{
+    }
+    //add digit da calculadora
+    addDigit(digit) {
+        //check if current operation already has a dot
+        if (digit === "." && this.currentOperationText.innerText.includes(".")){
+            return
+        }
+
+       this.currentOperation = digit;
+       this.updateScreen();
+       
+    }
+
+    //process all calculator operations
+    processOperation(operation) {
+        // get current and previuos value
+        let operationValue
+        let previous = +this.previousOperationText.innerText;
+        let current = +this.currentOperation.innerText;
+
+        switch(operation){
+            case "+":
+                break
+            default:
+                return;
+        }
+    }
+
+    //calculadora screen
+    updateScreen
+        (operationValue = null,
+        operation = null,
+        current = null,
+        previous)
+         {
+        this.currentOperationText.innerText += this.currentOperation;
+        
+        
+    }
+}
+
+const calc = new Calculator(previousOperationText, currentOperationText);
+
+//evento de pegar os buttons 
+buttons.forEach ((btn) => {
 
     btn.addEventListener('click', (e) => {
-
         const value = e.target.innerText;
 
-        console.log(value)
-    })
-})
+        if(+value >=0 || value === " . "){
+            calc.addDigit(value);
+        }else {
+            calc.processOperation(value)
+        }
+  
+    });
+});
 
 
  
